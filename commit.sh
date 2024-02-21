40b9029ac61af25fa9ad0bb1bad0184b55e326f7
Genkin <mengzhi.he@longbridge.sg>
2024-02-21 20:00:41 +0800

    trigger

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # diffs=$(git diff --numstat ticker.txt)
-diffs=$(git diff --numstat .github/workflows/sync.yml)
+diffs=$(git diff --numstat commit.sh)
 # 这里使用的是 master 分支，根据需求自行修改
 echo $diffs
 if [ "$diffs" == "" ];then
@@ -8,12 +8,13 @@ if [ "$diffs" == "" ];then
   exit 0
 fi
 
-changes=$(awk '{split($diffs,a," "); print(a[1]+a[2] < 100)}')
-
-if ["$changes" == "1"]; then
-    echo "error data"
-    exit 0
-else
+changes=$(git diff --numstat commit.sh | awk '{split($0,a," "); print(a[1]+a[2] < 100)}')
+echo $changes
+if [ "$changes" == "1" ]; then
+    echo "submit"
     git add .&&git commit -m "sync latest data" && git push origin main
+else
+    echo "data error"
+    exit 0
 fi
 
