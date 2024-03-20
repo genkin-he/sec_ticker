73398ec5e17dc9180f623a9327b4f1d8860801e9
Genkin <mengzhi.he@longbridge.sg>
2024-03-20 13:41:56 +0800

    fix commit script

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # diffs=$(git diff --numstat ticker.txt)
-diffs=$(git diff --numstat commit.sh)
+diffs=$(git diff)
 # 这里使用的是 master 分支，根据需求自行修改
 echo $diffs
 if [ "$diffs" == "" ];then
@@ -8,7 +8,17 @@ if [ "$diffs" == "" ];then
   exit 0
 fi
 
-changes=$(git diff --numstat commit.sh | awk '{split($0,a," "); print(a[1]+a[2] < 100)}')
+changes=$(git diff --numstat company_ticker.json | awk '{split($0,a," "); print(a[1]+a[2] < 100)}')
+echo $changes
+if [ "$changes" == "1" ]; then
+    echo "submit"
+    git add .&&git commit -m "sync latest data" && git push origin main
+else
+    echo "data error"
+    exit 0
+fi
+
+changes=$(git diff --numstat ticker.txt | awk '{split($0,a," "); print(a[1]+a[2] < 100)}')
 echo $changes
 if [ "$changes" == "1" ]; then
     echo "submit"
